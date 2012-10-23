af11087009e75bab3761181ce10968fb78498f8d
aki <lala.akira@gmail.com>
2012-10-23 22:12:43 +0900

    init tree

---
+++ fluent-plugin-flatten.gemspec
@@ -0,0 +1,23 @@
+# -*- encoding: utf-8 -*-
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+require 'fluent-plugin-flatten/version'
+
+Gem::Specification.new do |gem|
+  gem.name          = "fluent-plugin-flatten"
+  gem.version       = "0.0.1"
+  gem.authors       = ["aki"]
+  gem.email         = ["lala.akira@gmail.com"]
+  gem.description   = %q{Output filter plugin Flatten a nested json}
+  gem.summary       = %q{Output filter plugin Flatten a nested json}
+  gem.homepage      = "https://github.com/aki77/fluent-plugin-datacounter"
+
+  gem.files         = `git ls-files`.split($/)
+  gem.executables   = gem.files.grep(%r{^bin/}).map{ |f| File.basename(f) }
+  gem.test_files    = gem.files.grep(%r{^(test|spec|features)/})
+  gem.require_paths = ["lib"]
+
+  gem.add_development_dependency "rake"
+  gem.add_development_dependency "fluentd"
+  gem.add_runtime_dependency "fluentd"
+end
---
+++ lib/fluent/plugin/out_flatten.rb
@@ -0,0 +1,35 @@
+class Fluent::FlattenOutput < Fluent::Output
+  Fluent::Plugin.register_output('flatten', self)
+
+  config_param :tag,        :string, :default => 'flatten'
+  config_param :add_prefix, :string, :default => nil
+  config_param :delimiter,  :string, :default => '_'
+
+  def emit(tag, es, chain)
+    if @add_prefix
+      tag = @add_prefix + '.' + tag
+    else
+      tag = @tag
+    end
+
+    es.each do |time, record|
+      record = flatten(record, {}, :delimiter => @delimiter)
+      Fluent::Engine.emit(tag, time, record)
+    end
+
+    chain.next
+  end
+
+  def flatten(input = {}, output = {}, options = {})
+    delimiter = options[:delimiter] || "_"
+    input.each do |key, value|
+      key = options[:prefix].nil? ? "#{key}" : "#{options[:prefix]}#{delimiter}#{key}"
+      if value.is_a? Hash
+        make_hash_one_dimensional(value, output, :prefix => key, :delimiter => delimiter)
+      else
+        output[key]  = value
+      end
+    end
+    output
+  end
+end
